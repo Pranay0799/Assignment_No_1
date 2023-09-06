d46c47a290b32057226a0eebb293eaa06c7e3b32
Pranay0799 <90129528+Pranay0799@users.noreply.github.com>
2023-09-06 22:06:21 +0530

    Assignment_No_1_ Queston_3.py

--- Assignment_No_1_ Queston_3.py
+++ Assignment_No_1_ Queston_3.py
@@ -1,6 +1,12 @@
 # Write a Python program to count the number of even and odd numbers from a series of numbers.
 
-numbers = 1, 2, 3, 4, 5, 6, 7, 8, 9
+#Taking input from user as a list
+
+size = int(input("Enter no of elements you want to enter : "))
+numbers = []
+for i in range(size):
+    value = int(input(f"Enter {i+1} list elements : "))
+    numbers.append(value)
 
 odd_count = 0
 even_count = 0
